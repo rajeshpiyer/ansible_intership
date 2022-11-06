617cde9d2b5f53901089dc91a717dbb0ee1f44d1
root <root@ip-172-31-37-7.us-east-2.compute.internal>
2022-11-06 15:51:56 +0000

    a

--- make_inventory.py
+++ make_inventory.py
@@ -13,6 +13,8 @@ x = dataset['IPV4']
 
 # %%
 y = dataset['STREAM']
+a = dataset['USERNAME']
+b = dataset['PASSWORD']
 
 
 # %%
@@ -22,12 +24,12 @@ file1.write("[data_science]\n")
 count = len(x)
 for i in range(0,count):
   if y[i]=="data_science":
-    file1.write(str(x[i])+"\n")
+    file1.write(str(x[i])+" ansible_user="+str(a[i])+" ansible_ssh_pass="+str(b[i])+"\n")
 
 file1.write("[networking]\n")
 for i in range(0,count):
   if y[i]=="networking":
-    file1.write(str(x[i])+"\n")
+    file1.write(str(x[i])+" ansible_user="+str(a[i])+" ansible_ssh_pass="+str(b[i])+"\n")
 
 file1.write("[mca:children]\ndata_science\nnetworking")
 
